c8d1b06f1a990aa5883fb0ef11130844a6797858
Ba <ba.ousmane@satadsl.net>
2023-10-10 01:16:27 +0200

    initial commit banking microservice

--- MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,12 +1,12 @@
 ﻿using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using System;
-
+using Microsoft.Extensions.DependencyInjection;
 namespace MicroRabbit.Infra.IoC
 {
     public class DependencyContainer
     {
-        public static void RegisterService(IserviceCollection services)
+        public static void RegisterService(IServiceCollection services)
         {
             //Domain Bus
             services.AddTransient<IEventBus, RabbitMQBus>();
